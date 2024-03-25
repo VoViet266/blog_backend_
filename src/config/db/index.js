017282dceec010dae1cd886530b8e106b95cd61a
Võ Quốc Việt <119559241+VoViet266@users.noreply.github.com>
2024-03-25 09:45:23 +0700

    Update config/db/index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,8 +3,7 @@ const url = 'mongodb://127.0.0.1:27017/mydatabase';
  async function connect() {
     try{
         await mongoose.connect(url ,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+       
         });
         console.log('Connect successfully!!!');
     }
